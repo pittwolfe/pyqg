2c2233e9afe8732e95d6ea6474f851b56ad2f0bc
Navid C. Constantinou <navidcy@users.noreply.github.com>
2019-11-21 13:32:27 +1100

    Some more doc/comments fixes (#203)
    * fix typo in spectral ekman drag + some more polishin in layered equations
    
    * some polishing + typos in 2layer equations
    
    * some polishing in sqg and equivalent barotropic equations
    
    * fixes affiliations and website urls
    
    * fixes typo
    
    * fix units in comments
    
    * adds .DS_Store to gitignore
    
    * typo spectal -> spectral
    
    * typo in website url

--- pyqg/qg_model.py
+++ pyqg/qg_model.py
@@ -83,9 +83,9 @@ class QGModel(model.Model):
         delta : number
             Layer thickness ratio (H1/H2)
         U1 : number
-            Upper layer flow. Units: m/s
+            Upper layer flow. Units: meters seconds :sup:`-1`
         U2 : number
-            Lower layer flow. Units: m/s
+            Lower layer flow. Units: meters seconds :sup:`-1`
         """
 
         # physical
@@ -196,9 +196,9 @@ class QGModel(model.Model):
         ----------
 
         U1 : number
-            Upper layer flow. Units: m/s
+            Upper layer flow. Units: meters seconds :sup:`-1`
         U2 : number
-            Lower layer flow. Units: m/s
+            Lower layer flow. Units: meters seconds :sup:`-1`
         """
         self.U1 = U1
         self.U2 = U2
